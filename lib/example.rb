0c5deea9ab0fe7f466004d992435d8ddf05bd19f
Josh <josh.handon@gmail.com>
2020-11-10 18:13:45 -0500

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,16 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_hello_11x
+    n = 0
+    phrase = "Hello World"
+    while n <10
+        puts phrase
+        n = n + 1
+    end
+end
+
+def greeting
+    puts "Hello World"
+end
